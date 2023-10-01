ab2897ad92be2ece10340644f02066cd4a147477
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-10-01 19:34:50 +0900

    홈 화면 구성(네비게이션 바 & TableView)

---
+++ Catstagram/Home/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  Catstagram
+//
+//  Created by 임수진 on 2023/10/01.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+
+}
+
+extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
+}
